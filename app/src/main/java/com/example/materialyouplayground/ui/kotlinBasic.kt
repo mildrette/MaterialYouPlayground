16d0b0230cfe64bcfa13dec3f05a5523e450f44a
mildrette <mildredfonka1@maild.com>
2026-02-09 21:42:31 +0100

    learning conditionals and varaibles together, made a simple button that changes to dark app to dark and light mode using remember and mutableStateOf

--- app/src/main/java/com/example/materialyouplayground/ui/kotlinBasic.kt
+++ app/src/main/java/com/example/materialyouplayground/ui/kotlinBasic.kt
@@ -1,33 +1,77 @@
 package com.example.materialyouplayground.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun KotlinBasic(){
+fun KotlinBasic() {
 
-    val name = "Mildred"
-    var count = 0
-    count = 1
+    var isDark by remember { mutableStateOf(false) }
 
-    val isDark = true
-    val isLight = false
+    Column (modifier = Modifier.fillMaxSize()
+        .padding(25.dp)
+        .background(if(isDark) Color.Black else Color.White)
+    ){
 
-    if(isDark){
-        Text("this is dark mode")
-    } else {
-        Text("this is light mode")
+        Text("Pick and theme Below",
+            fontSize = 24.sp,
+            color = if (isDark) Color.White else Color.Black)
+
+        Button(onClick = {isDark = !isDark}) {
+            Text("Toggle Theme Here")
+        }
+
+        Spacer(modifier = Modifier.padding(16.dp))
+
+        Text(if (isDark) "This one is Dark Mode" else "This One is Light Mode",
+            fontSize = 24.sp, color = if (isDark) Color.White else Color.Black
+        )
     }
-
-
-
 }
-
 
 
 @Preview
 @Composable
 fun KotlinBasicPreview(){
-KotlinBasic()
+    KotlinBasic()
 }
+
+
+
+
+
+
+
+
+
+
+
+//    val name = "Mildred"
+//    var count = 0
+//    count = 1
+//
+//    val isDark = true
+//    val isLight = false
+//
+//    if(isDark){
+//        Text("this is dark mode")
+//    } else {
+//        Text("this is light mode")
+//    }
+
+
+
