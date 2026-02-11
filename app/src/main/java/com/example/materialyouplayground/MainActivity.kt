b8cc5b88300611e2950c42dd135f50bef6b5c692
mildrette <mildredfonka1@maild.com>
2026-02-11 14:34:19 +0100

    update and setting and mainscreen

--- app/src/main/java/com/example/materialyouplayground/MainActivity.kt
+++ app/src/main/java/com/example/materialyouplayground/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.materialyouplayground.ui.MainScreen
-import com.example.materialyouplayground.ui.theme.MyAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
